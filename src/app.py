9b697fadd74d9531884b439616589b0b061630e8
Minu <122645470+Minusuke@users.noreply.github.com>
2023-09-25 15:02:34 +0000

    funcionando

--- src/app.py
+++ src/app.py
@@ -4,32 +4,26 @@ app = Flask(__name__)
 
 todos = [
     {"label": "My first task", "done": False},
+    {"label": "My second task", "done": False}
 ]
 
-# GET TODOS
-
 @app.route('/todos', methods=['GET'])
-def hello_world():
-    text_json = jsonify(todos)
-    return text_json
-
-# POST TODOS
+def get_todos():
+    return jsonify(todos)
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
     todos.append(request_body)
-    text_json = jsonify(todos)
-    return text_json
-
-# DELETE TODOS
+    return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    todos.pop(position-1)
-    text_json = jsonify(todos)
-    return text_json 
-
+    if 0 <= position < len(todos):
+        del todos[position]
+        return jsonify(todos)
+    else:
+        return 'Invalid position', 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
